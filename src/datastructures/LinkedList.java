594a776d4151307a70e0635aa13cf33bdb5f80d4
Sam Coco <Samuel.HenriAC@outlook.com>
2019-04-15 23:50:21 +0100

    LinkedList implemented with add(e), get(i) and remove(i)

---
+++ src/datastructures/LinkedList.java
@@ -0,0 +1,93 @@
+package datastructures;
+
+public class LinkedList<T>{
+    private Node head;
+    private int size;
+
+    // inner node class
+    private class Node {
+        T data;
+        Node nextNode;
+    }
+
+    // linkedlist constructor
+    public LinkedList(){
+        head = new Node();
+        size = 0; // the number of elements in linkedlist
+    }
+
+    /**
+     * Adds data to the linkedlist.
+     * @param data - the data (element) to be added to the linkedlist
+     */
+    public void add(T data){
+            Node currentNode = head;
+            if(head.data == null){
+                head.data = data;
+                size++;
+            } else {
+                while(currentNode.nextNode != null){
+                    currentNode = currentNode.nextNode;
+                }
+                currentNode.nextNode = new Node();
+                currentNode.nextNode.data = data;
+                size++;
+            }
+        }
+
+    /**
+     * Gets element, at the specified index, of the linkedlist.
+     * @param index - the index of the desired element in the linkedlist
+     * @return the element at index, or null if index out of range
+     */
+    public T get(int index){
+            if(indexValid(index)){
+                Node currentNode = head;
+                int position = 0;
+                while(position != index){
+                    currentNode = currentNode.nextNode;
+                    position++;
+                }
+                System.out.println("LinkedList[" + index + "] = " + currentNode.data);
+                return currentNode.data;
+            } else {
+                return null;
+            }
+    }
+
+    public boolean indexValid(int index){
+        if(index >= 0 && index < size){
+            return true;
+        } else {
+            System.out.println("ERROR: " + index + "does not exist");
+            return false;
+        }
+    }
+
+    public void remove(int index){
+        if(indexValid(index)){
+            Node currentNode = head;
+            int position = 0;
+
+            // conditionals: index = 0, index = last index, index = any index but first and last
+            if(index == 0){
+                head = head.nextNode;
+                size--;
+            } else if(index == size - 1){
+                while(position != size - 2){ // size - 2 to get to node preceding node to be deleted
+                    currentNode = currentNode.nextNode;
+                    position++;
+                }
+                currentNode.nextNode = null;
+                size--;
+            } else {
+                while(position != index - 1){
+                    currentNode = currentNode.nextNode;
+                    position++;
+                }
+                currentNode.nextNode = currentNode.nextNode.nextNode;
+                size--;
+            }
+        }
+    }
+}

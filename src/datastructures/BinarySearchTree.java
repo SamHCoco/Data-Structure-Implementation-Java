de4b274b287448721478c6010e86b14cc56dfb02
Sam Coco <Samuel.HenriAC@outlook.com>
2019-05-03 04:29:17 +0100

    Preorder and Inorder traversal implemented

--- src/datastructures/BinarySearchTree.java
+++ src/datastructures/BinarySearchTree.java
@@ -1,8 +1,11 @@
 package datastructures;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class BinarySearchTree{
     private Node root;
-
+    private int size = 0;
     /**
      * Inner class defining Binary Search Tree nodes.
      */
@@ -38,12 +41,15 @@ public class BinarySearchTree{
         while (!inserted) {
             if (data > current_node.data && current_node.rightChild == null) {
                 current_node.rightChild = new Node(data);
+                size++;
                 inserted = true;
             } else if (data < current_node.data && current_node.leftChild == null) {
                 current_node.leftChild = new Node(data);
+                size++;
                 inserted = true;
             } else if (data.equals(current_node.data) && current_node.leftChild == null) {
                 current_node.leftChild = new Node(data);
+                size++;
                 inserted = true;
             } else if (data > current_node.data && current_node.rightChild != null) {
                 current_node = current_node.rightChild;
@@ -53,4 +59,116 @@ public class BinarySearchTree{
         }
     }
 
+    /**
+     * Traverses Binary Search Tree in preorder fashion (root, left, right) and returns the result.
+     * @return An ArrayList containing preorder traversal values for the Binary Search Tree.
+     */
+    public ArrayList<Double> preorderTraversal(){
+        Node currentNode = root;
+        ArrayList<Double> traverseValues = new ArrayList<>();
+        Stack<Node> binaryNodes = new Stack<>();
+        boolean traversed = false;
+        int counter = 0;
+        while(!traversed){
+            if(currentNode.leftChild != null && currentNode.rightChild != null){
+                if(!binaryNodes.contains(currentNode)){
+                    binaryNodes.push(currentNode);
+                    traverseValues.add(currentNode.data);
+                    counter++;
+                    currentNode = currentNode.leftChild;
+                } else {
+                    currentNode = currentNode.rightChild;
+                    binaryNodes.pop();
+                }
+            } else if(currentNode.leftChild != null && currentNode.rightChild == null){
+                traverseValues.add(currentNode.data);
+                counter++;
+                currentNode =currentNode.leftChild;
+            } else if(currentNode.leftChild == null && currentNode.rightChild != null){
+                traverseValues.add(currentNode.data);
+                counter++;
+                currentNode = currentNode.rightChild;
+            } else if(currentNode.leftChild == null && currentNode.rightChild == null){
+                traverseValues.add(currentNode.data);
+                counter++;
+                if(!binaryNodes.empty()){
+                    currentNode = binaryNodes.peek();
+                }
+            }
+            if(counter == size + 1){
+                traversed = true;
+            }
+        }
+        System.out.println("BST PREORDER TRAVERSAL: " + traverseValues);
+        return traverseValues;
+    }
+
+    /**
+     * Traverses Binary Search Tree in inorder fashion (left, root, right) and returns the result.
+     * @return An ArrayList containing inorder traversal values for the Binary Search Tree.
+     */
+    public ArrayList<Double> inorderTraversal(){
+        Node currentNode = root;
+        ArrayList<Double> traverseValues = new ArrayList<>();
+        Stack<Node> binaryNodes = new Stack<>();
+        Stack<Node> traversedNodes = new Stack<>();
+        Stack<Integer> zeroOneArray = new Stack<>();
+        Integer zeroOneCount = 0;
+        boolean traversed = false;
+        int counter = 0;
+        while(!traversed){
+            if(currentNode.leftChild != null && currentNode.rightChild != null){
+                if(zeroOneCount != 0){
+                    zeroOneArray.push(zeroOneCount);
+                    zeroOneCount = 0;
+                }
+                if(!binaryNodes.contains(currentNode)){
+                    binaryNodes.add(currentNode);
+                    currentNode = currentNode.leftChild;
+                } else {
+                    traverseValues.add(currentNode.data);
+                    counter++;
+                    currentNode = currentNode.rightChild;
+                    binaryNodes.pop();
+                }
+
+            } else if(currentNode.leftChild != null && currentNode.rightChild == null){
+                traversedNodes.add(currentNode);
+                zeroOneCount++;
+                currentNode = currentNode.leftChild;
+
+            } else if(currentNode.leftChild == null && currentNode.rightChild != null){
+                traversedNodes.add(currentNode);
+                zeroOneCount++;
+                currentNode = currentNode.rightChild;
+
+            } else if(currentNode.leftChild == null && currentNode.rightChild == null){
+                traversedNodes.add(currentNode);
+                zeroOneCount++;
+                zeroOneArray.push(zeroOneCount);
+                zeroOneCount = 0;
+                if(!binaryNodes.empty()){
+                    currentNode = binaryNodes.peek();
+                    for(int i = 0; i < zeroOneArray.peek(); i++){
+                        traverseValues.add(traversedNodes.pop().data);
+                        counter++;
+                    }
+                    zeroOneArray.pop();
+                } else {
+                    while(!zeroOneArray.empty()){
+                        for(int i = 0; i < zeroOneArray.peek(); i++){
+                            traverseValues.add(traversedNodes.pop().data);
+                            counter++;
+                        }
+                        zeroOneArray.pop();
+                    }
+                }
+            }
+            if(counter == size + 1){
+                traversed = true;
+            }
+        }
+        System.out.println("BST INORDER TRAVERSAL: " + traverseValues);
+        return traverseValues;
+    }
 }
